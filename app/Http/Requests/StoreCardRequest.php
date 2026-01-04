09d5c63b86759d50f49bbb3a71719d61326dfeb3
TrevinoRizkySunarja <you@example.com>
2026-01-04 13:37:10 +0100

    Version 1.4.1: - Login Auth fixed voor uploaden

--- app/Http/Requests/StoreCardRequest.php
+++ app/Http/Requests/StoreCardRequest.php
@@ -6,19 +6,12 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreCardRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        // Only logged-in users can create/update cards
+        return $this->user() !== null;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
